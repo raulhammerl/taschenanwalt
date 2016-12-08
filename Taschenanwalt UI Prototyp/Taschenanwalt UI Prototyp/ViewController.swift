c34c31c7356cc75acbb0842b7ac29e3284d3cdfb
Raul <raulhammerl@gmail.com>
2016-12-08 11:49:25 +0100

    Erster Designentwurf. Bisher nur die Landingpage.

---
+++ Taschenanwalt UI Prototyp/Taschenanwalt UI Prototyp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Taschenanwalt UI Prototyp
+//
+//  Created by King Kraul on 05/12/2016.
+//  Copyright © 2016 King Kraul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
