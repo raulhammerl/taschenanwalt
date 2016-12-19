bf04d79f34967e777cb34a3382ab4413e48527fb
admin <admin@admins-mbp.fritz.box>
2016-12-19 14:23:31 +0100

    Kamerafunktion

--- TA_UI/TA_UI/ViewController.swift
+++ TA_UI/TA_UI/ViewController.swift
@@ -8,8 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    /*
+    /* Buttons für Photo Library/Kamera öffnen/Speichern + ImageView zum Anzeigen des ausgewählten Bildes 
+     --> fehlt alles noch im Storyboard */
+    @IBOutlet weak var PhotoLibrary: UIButton!
+    @IBOutlet weak var Camera: UIButton!
+    @IBOutlet weak var Save: UIButton!
+    @IBOutlet weak var ImageDisplay: UIImageView! 
+     */
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,53 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    /*
+    /* Foto aus Photo Library auswählen */
+    @IBAction func PhotoLibraryAction(_ sender: UIButton) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary) {
+            
+            let picker = UIImagePickerController()
+            picker.delegate = self
+            picker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+            picker.allowsEditing = true;
+            self.present(picker, animated: true, completion: nil)
+            
+        }
+    }
+    
+    /* Kamera öffnen und neues Foto machen */
+    @IBAction func CameraAction(_ sender: UIButton) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+            
+            let picker = UIImagePickerController()
+            picker.delegate = self
+            picker.sourceType = UIImagePickerControllerSourceType.camera
+            picker.allowsEditing = false;
+            self.present(picker, animated: true, completion: nil)
+            
+        }
+    }
+    
+    /* Ausgewähltes Foto in Image View anzeigen */
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+        
+        ImageDisplay.image = image
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    /* Foto speichern */
+    @IBAction func SaveAction(_ sender: UIButton) {
+        
+        let image = UIImageJPEGRepresentation(ImageDisplay.image!, 0.6)
+        let compressedImage = UIImage(data: image!)
+        UIImageWriteToSavedPhotosAlbum(compressedImage!, nil, nil, nil)
+        
+    } 
+     */
+    
 
 }
 
