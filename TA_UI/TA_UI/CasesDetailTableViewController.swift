6b9b21f7fee73dcda896068f4b663b4c1fd6074b
Raul <raulhammerl@gmail.com>
2017-01-12 12:44:01 +0100

    Resolved storyboard conflict.

---
+++ TA_UI/TA_UI/CasesDetailTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CasesDetailTableViewController.swift
+//  TA_UI
+//
+//  Created by King Kraul on 11/01/2017.
+//  Copyright © 2017 Taschenanwalt. All rights reserved.
+//
+
+import Foundation
---
+++ TA_UI/TA_UI/DictionaryTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  DictionaryTableViewController.swift
+//  TA_UI
+//
+//  Created by King Kraul on 11/01/2017.
+//  Copyright © 2017 Taschenanwalt. All rights reserved.
+//
+
+import Foundation
