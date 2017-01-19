b6bb6019cd602bd1e664ffa671e5cd371e462232
admin <admin@admins-mbp.fritz.box>
2017-01-19 10:52:08 +0100

    Location and Camera Menu

--- TA_UI/TA_UI/Faelle.swift
+++ TA_UI/TA_UI/Faelle.swift
@@ -6,14 +6,39 @@ import UIKit
 
 class Faelle{
     
-    var wohnort: String;
-    var vorname:String;
     var identi: Int;
     
-    init(vorname:String, wohnort:String, identi: Int){
+    //Welcher Fall, zB Autounfall, Zugverspätung
+    //var art: String;
+
+    //Daten von
+    var vorname:String;
+    /*var nachname: String;
+    var strasse: String;
+    var hausnummer: String;
+    var plz: String;*/
+    var wohnort: String;
+   // var telefonnr: String;
+   // var kennzeichen: String;
+    
+    
+    var datum: String;
+    //var Unfallort: String;
+
+    /*var unfallHergang: String;
+    
+    var verletzte: Bool;
+    var sachschaden: Bool;
+    var autobahn: Bool;
+    var alkohol: Bool;
+    var ausland: Bool;*/
+    
+    init(vorname:String, wohnort:String, identi: Int, datum: String){
         self.vorname = vorname;
         self.wohnort = wohnort;
         self.identi = identi;
+        self.datum = datum;
+        
         
         // This is the call to the designated init.
         // defaulting to the Document directory. You can set this up to be any directory you want, or you can create convenience inits for both directories, if you want.
