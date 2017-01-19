a8a4fa686ac09afd5e4c59bd3fcf0751fe402930
Raul <raulhammerl@gmail.com>
2017-01-19 19:58:05 +0100

    DictionaryTableViewController Update

--- TA_UI/TA_UI/DictionaryTableViewController.swift
+++ TA_UI/TA_UI/DictionaryTableViewController.swift
@@ -6,4 +6,52 @@
 //  Copyright © 2017 Taschenanwalt. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class DictionaryTableViewCell: UITableViewCell {
+    @IBOutlet weak var DictionaryHeadline: UILabel!
+    
+}
+
+class DictionaryTableViewController: UITableViewController {
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.tableView.separatorStyle = .none
+        self.view.backgroundColor = UIColor(red: 230/255, green: 230/255, blue: 230/255, alpha: 1.0)
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+    
+    
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DictionaryTableViewCell", for: indexPath) as! DictionaryTableViewCell
+        
+        cell.DictionaryHeadline?.text = "Autounfall"
+        
+        return cell
+    }
+    
+    
+}
