19985658b48250ae2ea185bd663fe7826b99860b
Raul <raulhammerl@gmail.com>
2017-01-31 15:25:03 +0100

    APIRequest back to new

--- TA_UI/TA_UI/APIRequest.swift
+++ TA_UI/TA_UI/APIRequest.swift
@@ -16,17 +16,18 @@ class APIRequest
     //let urlEndpoint: String = "http://taschenanwalt.pythonanywhere.com/talk/?msg="
     //let urlEnvarint: String = "http://taschenanwalt.pythonanywhere.com/json/?msg="
     let baseUrl = "https://api.api.ai/v1/query?" //API.AI base url for api requests
-    let sessionId = 12345 //TODO: Generate a new session ID
+    //let sessionId = 12345 //TODO: Generate a new session ID
     let lang = "de" //German language
     let clientKey = "4315190c91b6490f8868b3acda1542ad" //API.AI client authentication key
     //var idHelper = 0;
-    func sendRequest(request: String, callback: @escaping (String) -> ())
+    func sendRequest(session: Int, request: String, callback: @escaping (String) -> ())
     {
         let escapedRequest = request.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed)
         //let url = URL(string: urlEnvarint + escapedRequest!) //create a url out of the endpoint
         let queryUrl = baseUrl+"query="+escapedRequest!
         //let completeUrl = baseUrl + "query="+escapedRequest!+"&lang="+lang+"&sessionId="+sessionId
-        let completeUrl = queryUrl+"&lang="+lang+"&sessionId="+String(sessionId)
+        //let completeUrl = queryUrl+"&lang="+lang+"&sessionId="+String(sessionId)
+        let completeUrl = queryUrl+"&lang="+lang+"&sessionId="+String(session)
         let url = URL(string: completeUrl)
         print(request);
         //print("Url endpoint + request: " + urlEnvarint + escapedRequest!);
@@ -76,15 +77,6 @@ class APIRequest
                     if let sachschadenJson = json["result"]["parameters"]["sachschaden"].string{
                         self.fall.sachschaden = sachschadenJson;
                     }
-                    if let alkoholJson = json["result"]["parameters"]["alkohol"].string{
-                        self.fall.alkohol = alkoholJson;
-                    }
-                    if let auslandJson = json["result"]["parameters"]["ausland"].string{
-                        self.fall.ausland = auslandJson;
-                    }
-                    if let autobahnJson = json["result"]["parameters"]["autobahn"].string{
-                        self.fall.autobahn = autobahnJson;
-                    }
                     if let nameJson = json["result"]["parameters"]["name"].string{
                         self.fall.name = nameJson;
                     }
@@ -102,7 +94,7 @@ class APIRequest
                     }
                     
                     if let done = json["result"]["parameters"]["done"].string{
-                        if(done == "" ){
+                        if(done == "" || done == "done"){
                             self.saveInJsonAutounfall(autounfall: self.fall);
                         }
                     }
